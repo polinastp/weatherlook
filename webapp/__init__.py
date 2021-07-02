9381f36a6e0eb6d588c83ceefa00d467ac2abb92
Polina Stepanova <stepa.polina24@gmail.com>
2021-07-02 11:39:31 +0300

    Все работает

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from webapp.forms import LoginForm
 
 def create_app():
-    app = Flask(__name__, template_folder={})
+    app = Flask(__name__)
     app.config.from_pyfile('config.py')
 
     @app.route('/')
