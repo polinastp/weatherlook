a250d907d2f3f93f692c95745b63acfb0cf82f58
Polina Stepanova <stepa.polina24@gmail.com>
2021-07-14 12:35:56 +0300

    Fixed loader.py (#18)
    * Fixed loader.py
    
    * fixed read_csv

--- loader.py
+++ loader.py
@@ -6,7 +6,9 @@ from webapp.db import db_session
 
 def save_data(row):
     clothes = Clothes(name=row['name'], gender=row['gender'],
-    cloth_type=row['cloth_type'], temp_tag=row['temp_tag'].split(';'), 
+    cloth_type=row['cloth_type'], temp_freezing=bool(int(row['temp_freezing'])),
+    temp_cold=bool(int(row['temp_cold'])), temp_chilly=bool(int(row['temp_chilly'])),
+    temp_warm=bool(int(row['temp_warm'])), temp_hot=bool(int(row['temp_hot'])),
     rain=bool(int(row['rain'])), sun=bool(int(row['sun'])), icon_path=row['icon_path'])
     
     db_session.add(clothes)
@@ -14,12 +16,13 @@ def save_data(row):
 
 def read_csv(filename):
     with open(filename, 'r', encoding='utf-8') as f:
-        fields = ['name', 'gender', 'cloth_type', 'temp_tag',
-         'rain', 'sun', 'icon_path']
+        fields = ['name', 'gender', 'cloth_type', 'temp_freezing',
+        'temp_cold', 'temp_chilly', 'temp_warm', 'temp_hot',
+        'rain', 'sun', 'icon_path']
         reader = csv.DictReader(f, fields, delimiter =',')
         for row in reader:
             save_data(row)
 
     
 if __name__ == "__main__":
-    read_csv('weatherlook.csv')
+    read_csv('weatherlook1.csv')
